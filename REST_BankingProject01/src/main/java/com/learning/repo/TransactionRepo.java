27eb399874c0936e95e47c2d59e00caec9d77a0e
StabbedToDeath <pranavdadu@gmail.com>
2022-07-22 10:24:18 -0400

    Delete REST_BankingProject01 directory

--- REST_BankingProject01/src/main/java/com/learning/repo/TransactionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.learning.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.learning.entity.Transaction;
-
-public interface TransactionRepo extends JpaRepository<Transaction, Integer> {
-
-}
